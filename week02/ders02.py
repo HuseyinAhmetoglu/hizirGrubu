27e5b6f58e5ebc7a2a49535fa73303ea79ebeed0
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-04-30 21:38:53 +0300

    commit

---
+++ week02/ders02.py
@@ -0,0 +1,7 @@
+ad = 'hüseyin ahmetoğlu'
+print(ad)
+print(ad.title())
+print(ad.upper())
+ad2 = "ALİ VELİOĞLU"
+print(ad2.lower())
+
---
+++ week02/ders1.py
@@ -0,0 +1,5 @@
+mesaj = "Merhaba Python!"
+ikinci_mesaj = "Selam"
+#3.mesaj = "İyi günler"
+mesaj3 = "İyi günler"
+print(mesaj3)
