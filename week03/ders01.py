a0f1fc1c604b208264390e2b6b37a35b74cb7b3f
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-05-05 21:51:24 +0300

    commit

---
+++ week03/ders01.py
@@ -0,0 +1,13 @@
+isimler = ["enes", "mehmet", "nursena", "ensar"]
+print(isimler)
+print(isimler[0])
+print(isimler[0].title())
+print(isimler[1])
+print(isimler[1].upper())
+print(isimler[-1])
+print(isimler[3])
+
+mesaj = f"Sınıfın en çalışkan öğrencisi {isimler[0].title()}"
+print(mesaj)
+
+
